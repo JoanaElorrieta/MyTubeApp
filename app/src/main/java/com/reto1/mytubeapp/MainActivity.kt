9b3d0615c7b853188ba5af3bbece6c8ee1271359
Joana <joana.barbermo@elorrieta-errekamari.com>
2023-10-18 17:42:26 +0200

    Pantalla Registro borrador, navegable hasta la pantalla de Canciones no funcional

--- app/src/main/java/com/reto1/mytubeapp/MainActivity.kt
+++ app/src/main/java/com/reto1/mytubeapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.reto1.mytubeapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Looper
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Handler
 import android.widget.ImageView
 import android.widget.TextView
+import com.reto1.mytubeapp.song.SongActivity
+import com.reto1.mytubeapp.user.RegisterActivity
 
 
 class MainActivity :  AppCompatActivity() {
@@ -16,13 +18,23 @@ class MainActivity :  AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         handler.postDelayed({ logIn() }, 3000)
 
-
+        findViewById<Button>(R.id.register).setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
+        findViewById<Button>(R.id.login).setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.notNow).setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
     fun logIn(){
-        Log.d("Etiqueta", "HOLA")
         findViewById<ImageView>(R.id.logoInicio).visibility = View.INVISIBLE
         findViewById<ImageView>(R.id.logoText).visibility = View.VISIBLE
         findViewById<TextView>(R.id.password).visibility = View.VISIBLE
@@ -32,4 +44,6 @@ class MainActivity :  AppCompatActivity() {
         findViewById<Button>(R.id.register).visibility = View.VISIBLE
         findViewById<Button>(R.id.notNow).visibility = View.VISIBLE
     }
-    }
+
+
+}
---
+++ app/src/main/java/com/reto1/mytubeapp/song/SongActivity.kt
@@ -0,0 +1,15 @@
+package com.reto1.mytubeapp.song
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+import com.reto1.mytubeapp.R
+
+class SongActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.song_activity)
+
+
+    }
+}
